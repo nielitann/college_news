69e0a5558add84f3c02dd1f12d48739fbb253568
nielitann <cquest648@gmail.com>
2025-06-05 11:35:49 +0300

    server1.py

--- server1.py
+++ server1.py
@@ -373,6 +373,6 @@ def get_news_by_id(news_id):
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
